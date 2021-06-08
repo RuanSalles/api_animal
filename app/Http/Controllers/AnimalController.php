093c7fda291cf8efe4e562e26352e95d148fb8ce
RuanSales <contatoruansales@gmail.com>
2021-06-08 15:39:37 -0300

    Primeiro Commit

---
+++ app/Http/Controllers/AnimalController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Animal;
+use Illuminate\Http\Request;
+
+class AnimalController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $getall = Animal::all();
+        return $getall;
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $animal = Animal::create($request->all());
+        return $animal;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Animal  $animal
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Animal $animal)
+    {
+        $animal = Animal::findOrFail($animal->id);
+        return $animal;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Animal  $animal
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Animal $animal)
+    {
+        $animal = Animal::findOrFail($animal->id)->update($request->all());
+        return $animal;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Animal  $animal
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Animal $animal)
+    {
+        $animal = Animal::destroy($animal->id);
+                
+    }
+}
